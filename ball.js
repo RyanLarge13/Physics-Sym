de36a57298203162594f2ac025d991487b49e5b2
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2024-06-04 10:02:16 -0400

    Mass
    Fixing some bugs and adding mass into the equation

--- ball.js
+++ ball.js
@@ -7,11 +7,13 @@ class Ball {
   this.fill = fill;
   this.velocityY = 0;
   this.velocityX = 0;
-  this.mass = (4 / 3) * Math.PI * Math.pow(r, 3);
+  // commented out mass is a real life calculation not suitable for my non scaled program instead let's device the radius by 10
+  // this.mass = (4 / 3) * Math.PI * Math.pow(r, 3);
+  this.mass = r / 10;
   this.restitution = 1;
   this.dampingY = 0.9;
   this.dampingX = 0.9;
-  this.gravity = 0.1;
+  this.gravity = 0.05;
   this.friction = 0.01;
   this.isDragging = false;
   this.tiltSensitivity = 0.00075;
@@ -24,18 +26,23 @@ class Ball {
   this.sym.ctx.closePath();
  }
  update(tilt) {
+  // We only want to run the physics on balls we are not dragging around the screen
   if (!this.isDragging) {
+   // Handle Y physics and collisions
    if (this.y + this.r >= this.sym.height) {
     this.y = this.sym.height - this.r;
+    // reverse the direction of ball when hitting the ground for bounce effect apply damping effect to decrease balls kenetic energy
     this.velocityY *= -this.dampingY;
     this.dampingY -= Math.abs((0.1 * this.r) / 100);
-    if (Math.abs(this.velocityY) < 0.1) {
+    // if the velocity of the ball has decreased to almost 0 we set the velocity to 0 to settle and stop ball movement
+    if (Math.abs(this.velocityY) <= 0.1) {
      this.velocityY = 0;
      this.dampingY = 0.9;
     }
    } else {
     this.velocityY += this.gravity;
    }
+   // Handle X physics & collisions
    if (this.x + this.r >= this.sym.width || this.x - this.r <= 0) {
     if (this.x + this.r > this.sym.width) {
      this.x = this.sym.width - this.r;
@@ -44,7 +51,8 @@ class Ball {
      this.x = 0 + this.r;
     }
     this.velocityX *= -this.dampingX;
-    this.dampingX -= Math.abs((0.1 * this.r) / 100);
+    //this.dampingX -= Math.abs((0.01 * this.r) / 100);
+    this.dampingX -= this.friction;
     if (Math.abs(this.velocityX) < 0.1) {
      this.velocityX = 0;
      this.dampingX = 0.9;
@@ -63,9 +71,9 @@ class Ball {
     }
    }
    // Stop tilt for now
-   if (tilt) {
+   /* if (tilt) {
     this.velocityX += -(Math.floor(tilt) - 90) * this.tiltSensitivity;
-   }
+   } */
    this.y += this.velocityY;
    this.x += this.velocityX;
   }
--- script.js
+++ script.js
@@ -1,123 +1,4 @@
-import Ball from "./ball.js";
-
-let WIDTH;
-let HEIGHT;
-
-class Sym {
- constructor(canvas, context) {
-  this.canvas = canvas;
-  this.ctx = context;
-  this.width = this.canvas.width;
-  this.height = this.canvas.height;
-  this.rect = this.canvas.getBoundingClientRect();
-  this.selectedBall = null;
-  this.tilt = 0;
-  this.prevPosY = null;
-  this.prevPosX = null;
-  this.balls = [new Ball(this, this.width / 2, this.height / 2, 25, "#EEEEEE")];
-  this.canvas.addEventListener("touchstart", this.selectBall.bind(this));
-  this.canvas.addEventListener("touchmove", this.moveBall.bind(this));
-  this.canvas.addEventListener("touchend", this.deSelectBall.bind(this));
- }
- clear() {
-  this.ctx.clearRect(0, 0, this.width, this.height);
- }
- drawBalls() {
-  for (let i = 0; i < this.balls.length; i++) {
-   this.balls[i].draw();
-   for (let j = i + 1; j < this.balls.length; j++) {
-    const ballA = this.balls[i];
-    const ballB = this.balls[j];
-    const posA = ballA.getPos();
-    const posB = ballB.getPos();
-    const xDiff = posB.x - posA.x;
-    const yDiff = posB.y - posA.y;
-    const distance = Math.sqrt(xDiff * xDiff + yDiff * yDiff);
-    const minDistance = posA.r + posB.r;
-    if (distance < minDistance) {
-     const normalX = xDiff / distance;
-     const normalY = yDiff / distance;
-     const relativeVelocityX = ballA.velocityX - ballB.velocityX;
-     const relativeVelocityY = ballA.velocityY - ballB.velocityY;
-     const velocityAlongNormal =
-      relativeVelocityX * normalX + relativeVelocityY * normalY;
-     // Use when implementing more realism
-     // const restitution = (ballA.restitution + ballB.restitution) / 2;
-     const restitution = 1;
-     const impulse = (-(1 + restitution) * velocityAlongNormal) / 2;
-     ballA.velocityX += impulse * normalX;
-     ballA.velocityY += impulse * normalY;
-     ballB.velocityX -= impulse * normalX;
-     ballB.velocityY -= impulse * normalY;
-     const overlap = minDistance - distance;
-     const correctionFactor = 1;
-     const correctionX = overlap * normalX * correctionFactor;
-     const correctionY = overlap * normalY * correctionFactor;
-     ballA.x -= correctionX;
-     ballA.y -= correctionY;
-     ballB.x += correctionX;
-     ballB.y += correctionY;
-    }
-   }
-   if (!this.balls[i].isDragging) {
-    this.balls[i].update(this.tilt);
-   }
-  }
- }
- createBall(x, y, r = 25, fill = "#EEEEEE") {
-  const newBall = new Ball(this, x, y, r, fill);
-  this.balls.push(newBall);
- }
- selectBall(e) {
-  e.preventDefault();
-  e.stopPropagation();
-  const clientX = e.touches[0].clientX - this.rect.left;
-  const clientY = e.touches[0].clientY - this.rect.top;
-  for (let i = 0; i < this.balls.length; i++) {
-   const { x, y, r } = this.balls[i].getPos();
-   const dis = Math.sqrt((clientX - x) ** 2 + (clientY - y) ** 2);
-   if (dis <= r) {
-    this.selectedBall = this.balls[i];
-    this.selectedBall.isDragging = true;
-    this.selectedBall.velocity = 0;
-    this.selectedBall.damping = 0.9;
-    this.selectedBall.move(clientX, clientY);
-    break;
-   }
-  }
- }
- moveBall(e) {
-  e.preventDefault();
-  e.stopPropagation();
-  const clientX = e.touches[0].clientX - this.rect.left;
-  const clientY = e.touches[0].clientY - this.rect.top;
-  let deltaY = 0;
-  let deltaX = 0;
-  if (this.prevPosY !== null) {
-   deltaY = clientY - this.prevPosY;
-  }
-  if (this.prevPosX !== null) {
-   deltaX = clientX - this.prevPosX;
-  }
-  const currentTime = performance.now();
-  const deltaTime = currentTime - this.prevTime;
-  const velocityFactor = 0.2;
-  if (this.selectedBall !== null) {
-   this.selectedBall.velocityY += (deltaY * velocityFactor) / deltaTime;
-   this.selectedBall.velocityX += (deltaX * velocityFactor) / deltaTime;
-   this.selectedBall.move(clientX, clientY);
-  }
-  this.prevPosY = clientY;
-  this.prevPosX = clientX;
-  this.prevTime = currentTime;
- }
- deSelectBall() {
-  if (this.selectedBall !== null) {
-   this.selectedBall.isDragging = false;
-   this.selectedBall = null;
-  }
- }
-}
+import Sym from "./sym.js";
 
 const sizeCanvas = (canvas, w, h) => {
  canvas.width = w;
@@ -158,10 +39,10 @@ window.addEventListener("load", () => {
   sym.drawBalls();
   requestAnimationFrame(animate);
  };
- for (let i = 0; i < 40; i++) {
+ for (let i = 0; i < 25; i++) {
   const randX = Math.floor(Math.random() * sym.width);
   const randY = Math.floor(Math.random() * 100);
-  const randSize = Math.floor(Math.random() * 50);
+  const randSize = Math.floor(Math.random() * 40);
   const randColor = Math.floor(Math.random() * 10);
   sym.createBall(randX, randY, randSize, `#f${randColor}f`);
  }
