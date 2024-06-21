bbb31f499d0f62bbb15f26b71926cb833e295b22
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2024-06-21 19:18:12 -0400

    Slowly Improving
    Improvements in this commit include:
    - Setting maximum and minimum x y velocities
    - No correction factor when hitting bottom and sides of screen
    
    Unfortunatley still running into performance issues
    and I believe this is why the simulation
    merges balls together and causes chaotic
    movements under high pressures

--- ball.js
+++ ball.js
@@ -1,92 +1,99 @@
 class Ball {
-  constructor(sym, x, y, r = 25, fill = "#EEEEEE") {
-    this.sym = sym;
-    this.x = x;
-    this.y = y;
-    this.r = r;
-    this.fill = fill;
-    this.velocityY = 0.1;
-    this.velocityX = 0;
-    // commented out mass is a real life calculation not suitable for my non scaled program instead let's device the radius by 10
-    // this.mass = (4 / 3) * Math.PI * Math.pow(r, 3);
-    this.mass = r / 10;
-    // this.restitution = 1;
-    this.dampingY = 0.9;
-    this.gravity = 0.05;
-    this.friction = 0.005;
-    this.isDragging = false;
-    this.tiltSensitivity = -0.0005;
-  }
-  draw() {
-    this.sym.ctx.beginPath();
-    this.sym.ctx.arc(this.x, this.y, this.r, Math.PI * 2, false);
-    //Instead let's change color based on Y velocity. First we normalize the y velocity with min, max & current values
-    // this.sym.ctx.fillStyle = this.fill;
-    // let VNormal = this.velocityY / 5;
-    // VNormal = Math.max(0, Math.min(1, VNormal));
-    // const R = Math.round(VNormal * 255);
-    // const G = 0;
-    // const B = Math.round((1 - VNormal) * 255);
-    const velCum = Math.abs(this.velocityY) + Math.abs(this.velocityX);
-    const R = Math.min(255, Math.round(velCum * 50));
-    const G = 0;
-    const B = 255;
-    const hex = `#${R.toString(16).padStart(2, "0")}${G.toString(16).padStart(
-      2,
-      "0"
-    )}${B.toString(16).padStart(2, "0")}`;
-    this.sym.ctx.fillStyle = hex.toUpperCase();
-    this.sym.ctx.fill();
-    this.sym.ctx.closePath();
-  }
-  update(tilt) {
-    // We only want to run the physics on balls we are not dragging around the screen
-    if (!this.isDragging) {
-      // Handle Y physics and collisions
-      this.velocityY += this.gravity;
-      if (this.y + this.r >= this.sym.height) {
-        this.y = this.sym.height - this.r;
-        // reverse the direction of ball when hitting the ground for bounce effect apply damping effect to decrease balls kinetic energy
-        this.velocityY *= -this.dampingY;
-        this.dampingY -= Math.abs((0.1 * this.r) / 100);
-        // if the velocity of the ball has decreased to almost 0 we set the velocity to 0 to settle and stop ball movement
-        if (Math.abs(this.velocityY) < 0.1) {
-          this.velocityY = 0;
-          this.dampingY = 0.9;
-        }
-      }
-      // Handle X physics & collisions
-      if (Math.abs(this.velocityX) <= 0.1) {
-        this.velocityX = 0;
-      }
-      if (this.x + this.r >= this.sym.width) {
-        this.velocityX *= -1;
-        this.x = this.sym.width - this.r;
-      }
-      if (this.x - this.r <= 0) {
-        this.velocityX *= -1;
-        this.x = 0 + this.r;
-      }
-      if (this.velocityX > 0) {
-        this.velocityX -= this.friction;
-      } else if (this.velocityX < 0) {
-        this.velocityX += this.friction;
-      }
-      // Stop tilt for now
-      if (tilt) {
-        // this.velocityX += -(Math.floor(tilt) - 90) * this.tiltSensitivity;
-      }
-      this.y += this.velocityY;
-      this.x += this.velocityX;
+ constructor(sym, x, y, r = 25, fill = "#EEEEEE") {
+  this.sym = sym;
+  this.x = x;
+  this.y = y;
+  this.r = r;
+  this.fill = fill;
+  this.velocityY = 0.25;
+  this.velocityX = 0;
+  // commented out mass is a real life calculation not suitable for my non scaled program instead let's device the radius by 10
+  // this.mass = (4 / 3) * Math.PI * Math.pow(r, 3);
+  this.mass = r / 10;
+  // this.restitution = 1;
+  this.dampingY = 0.9;
+  this.gravity = 0.05;
+  this.friction = 0.005;
+  this.isDragging = false;
+  this.tiltSensitivity = -0.0005;
+ }
+ draw() {
+  this.sym.ctx.beginPath();
+  this.sym.ctx.arc(this.x, this.y, this.r, Math.PI * 2, false);
+  //Instead let's change color based on Y velocity. First we normalize the y velocity with min, max & current values
+  // this.sym.ctx.fillStyle = this.fill;
+  // let VNormal = this.velocityY / 5;
+  // VNormal = Math.max(0, Math.min(1, VNormal));
+  // const R = Math.round(VNormal * 255);
+  // const G = 0;
+  // const B = Math.round((1 - VNormal) * 255);
+  const velCum = Math.abs(this.velocityY) + Math.abs(this.velocityX);
+  const R = Math.min(255, Math.round(velCum * 50));
+  const G = 0;
+  const B = 255;
+  const hex = `#${R.toString(16).padStart(2, "0")}${G.toString(16).padStart(
+   2,
+   "0"
+  )}${B.toString(16).padStart(2, "0")}`;
+  this.sym.ctx.fillStyle = hex.toUpperCase();
+  this.sym.ctx.fill();
+  this.sym.ctx.closePath();
+ }
+ update(tilt) {
+  // We only want to run the physics on balls we are not dragging around the screen
+  if (!this.isDragging) {
+   // Handle Y physics and collisions
+   if (Math.abs(this.velocityY < 4)) {
+    this.velocityY += this.gravity;
+   }
+   if (this.y + this.r >= this.sym.height) {
+    this.y = this.sym.height - this.r;
+    // reverse the direction of ball when hitting the ground for bounce effect apply damping effect to decrease balls kinetic energy
+    this.velocityY *= -this.dampingY;
+    this.dampingY -= Math.abs((0.1 * this.r) / 100);
+    // if the velocity of the ball has decreased to almost 0 we set the velocity to 0 to settle and stop ball movement
+    if (Math.abs(this.velocityY) < 0.1) {
+     this.velocityY = 0;
+     this.dampingY = 0.9;
     }
+   }
+   // Handle X physics & collisions
+   if (Math.abs(this.velocityX) <= 0.1) {
+    this.velocityX = 0;
+   }
+   if (this.x + this.r >= this.sym.width) {
+    this.velocityX *= -1;
+    // this.x = this.sym.width - this.r;
+   }
+   if (this.x - this.r <= 0) {
+    this.velocityX *= -1;
+    //this.x = 0 + this.r;
+   }
+   if (this.velocityX > 0) {
+    this.velocityX -= Math.abs((0.1 * this.r) / 100);
+   }
+   if (this.velocityX < 0) {
+    this.velocityX += Math.abs((0.1 * this.r) / 100);
+   }
+   // Stop tilt for now
+   if (tilt) {
+    if (Math.abs(this.velocityX < 4)) {
+    this.velocityX += (Math.floor(tilt) - 90) * this.tiltSensitivity;
+    } else {
+     this.velocityX = Math.sign(this.velocityX) * 4;
+    }
+   }
+   this.y += this.velocityY;
+   this.x += this.velocityX;
   }
-  move(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-  getPos() {
-    return { x: this.x, y: this.y, r: this.r };
-  }
+ }
+ move(x, y) {
+  this.x = x;
+  this.y = y;
+ }
+ getPos() {
+  return { x: this.x, y: this.y, r: this.r };
+ }
 }
 
 export default Ball;
--- script.js
+++ script.js
@@ -1,59 +1,63 @@
 import Sym from "./sym.js";
 
 const sizeCanvas = (canvas, w, h) => {
-  canvas.width = w;
-  canvas.height = h;
+ canvas.width = w;
+ canvas.height = h;
 };
 
 window.addEventListener("load", () => {
-  const canvas = document.getElementById("canvas");
-  const ctx = canvas.getContext("2d");
-  const winWidth = window.innerWidth;
-  const winHeight = window.innerHeight;
-  canvas.width = winWidth;
-  canvas.height = winHeight;
-  const sym = new Sym(canvas, ctx);
-  window.addEventListener("resize", () => {
-    const w = window.innerWidth;
-    const h = window.innerHeight;
-    sizeCanvas(canvas, w, h);
-    sym.width = w;
-    sym.height = h;
-    sym.initializeGrid();
-  });
-  if (window.DeviceOrientationEvent) {
-    window.addEventListener("deviceorientation", (e) => {
-      sym.tilt = e.alpha;
-    });
-  }
-  if (screen.orientation && screen.orientation.lock) {
-    screen.orientation
-      .lock("portrait")
-      .then(() => console.log("locked"))
-      .catch((err) => console.log(err, "not locked"));
-  }
-  let prevT = 0;
-  const animate = (time) => {
-    const deltaT = time - prevT;
-    prevT = time;
-    sym.clear();
-    sym.drawBalls();
-    requestAnimationFrame(animate);
-  };
-  for (let i = 0; i < 3000; i++) {
-    const randX = Math.max(
-      5,
-      Math.min(sym.width - 5, Math.random() * (sym.width - 10) + 5)
-    );
-    const randY = Math.max(
-      5,
-      Math.min(sym.height - 5, Math.random() * (sym.height - 10) + 5)
-    );
-    const randSize = Math.floor(Math.random() * 40);
-    const randColor = Math.floor(Math.random() * 10);
-    sym.createBall(randX, randY, 5, `#f${randColor}f`);
-  }
+ const canvas = document.getElementById("canvas");
+ const ctx = canvas.getContext("2d");
+ const winWidth = window.innerWidth;
+ const winHeight = window.innerHeight;
+ canvas.width = winWidth;
+ canvas.height = winHeight;
+ const sym = new Sym(canvas, ctx);
+ window.addEventListener("resize", () => {
+  const w = window.innerWidth;
+  const h = window.innerHeight;
+  sizeCanvas(canvas, w, h);
+  sym.width = w;
+  sym.height = h;
   sym.initializeGrid();
-  // sym.initializeWorkers();
-  animate();
+ });
+ if (window.DeviceOrientationEvent) {
+  window.addEventListener("deviceorientation", e => {
+   sym.tilt = e.alpha;
+  });
+ }
+ if (screen.orientation && screen.orientation.lock) {
+  screen.orientation
+   .lock("portrait")
+   .then(() => console.log("locked"))
+   .catch(err => console.log(err, "not locked"));
+ }
+ let prevT = 0;
+ const animate = time => {
+  const deltaT = time - prevT;
+  prevT = time;
+  sym.clear();
+  sym.drawBalls();
+  requestAnimationFrame(animate);
+ };
+ for (let i = 0; i < 2000; i++) {
+  const randX = Math.max(
+   5,
+   Math.min(sym.width - 5, Math.random() * (sym.width - 10) + 5)
+  );
+  // const randY = Math.max(
+  //    5,
+  //    Math.min(sym.height - 5, Math.random() * (sym.height - 10) + 5)
+  //   );
+  const randY = Math.max(
+   5,
+   Math.min(sym.height - 5, Math.random() * (sym.height - 10) + 5)
+  );
+  const randSize = Math.floor(Math.random() * 40);
+  const randColor = Math.floor(Math.random() * 10);
+  sym.createBall(randX, randY, 5, `#f${randColor}f`);
+ }
+ sym.initializeGrid();
+ // sym.initializeWorkers();
+ animate();
 });
